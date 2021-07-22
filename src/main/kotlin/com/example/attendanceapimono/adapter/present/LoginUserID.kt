83a352e517351767862b68eb24d6232bd3abb4d5
Lee Jaeseong <fg9658@gmail.com>
2021-07-23 01:15:22 +0900

    feat(security, identify): 유저 아이디 어노테이션 추가

---
+++ src/main/kotlin/com/example/attendanceapimono/adapter/present/LoginUserID.kt
@@ -0,0 +1,6 @@
+package com.example.attendanceapimono.adapter.present
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+annotation class LoginUserID;
