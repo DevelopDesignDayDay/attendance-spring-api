8c2a19275439d74be371544abbc907d8967f691a
Lee Jaeseong <fg9658@gmail.com>
2021-07-23 01:14:24 +0900

    feat(service, repository): 유저 서비스 및 유저 레파지토리 생성

---
+++ src/main/kotlin/com/example/attendanceapimono/application/UserService.kt
@@ -0,0 +1,8 @@
+package com.example.attendanceapimono.application
+
+import com.example.attendanceapimono.domain.user.UserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class UserService(private val userRepository: UserRepository) {
+}
