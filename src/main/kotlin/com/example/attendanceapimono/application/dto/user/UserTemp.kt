bc30cb9c07f48dcc6b05d800092822e46b2dc0b1
Lee Jaeseong <fg9658@gmail.com>
2021-07-29 00:28:30 +0900

    chore: 쓸때 없는 파일 제거 및 불 필요한 브라켓 제거

--- src/main/kotlin/com/example/attendanceapimono/application/dto/user/UserTemp.kt
+++
@@ -1,4 +0,0 @@
-package com.example.attendanceapimono.application.dto.user
-
-class UserTemp {
-}
--- src/main/kotlin/com/example/attendanceapimono/domain/user/UserRepository.kt
+++ src/main/kotlin/com/example/attendanceapimono/domain/user/UserRepository.kt
@@ -5,5 +5,4 @@ import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-interface  UserRepository : JpaRepository<User, UUID> {
-}
+interface  UserRepository : JpaRepository<User, UUID>
