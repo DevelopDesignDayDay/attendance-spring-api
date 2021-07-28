0099349141bb00c182a3d9ac961fc83abf79f98b
Lee Jaeseong <fg9658@gmail.com>
2021-07-29 00:37:52 +0900

    feat(google-signup): bind controller

--- src/main/kotlin/com/example/attendanceapimono/adapter/present/api/UserAPI.kt
+++ src/main/kotlin/com/example/attendanceapimono/adapter/present/api/UserAPI.kt
@@ -1,4 +1,39 @@
 package com.example.attendanceapimono.adapter.present.api
 
+import com.example.attendanceapimono.application.dto.user.CreateUser
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import reactor.core.publisher.Mono
+import javax.validation.Valid
+import io.swagger.v3.oas.annotations.parameters.RequestBody as DocRequestBody
+
+@Tag(name = "유저 관련 API")
 interface UserAPI {
+
+    @Operation(
+        summary = "유저 생성",
+        requestBody = DocRequestBody(content = [
+            Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = CreateUser::class),
+            )
+        ])
+    )
+    @ApiResponse(
+        responseCode = "201",
+        description = "일반 참가자 생성",
+        content = [Content(examples = [ExampleObject("")])],
+    )
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/user")
+    suspend fun createUser(@Valid @RequestBody body: Mono<CreateUser>)
 }
---
+++ src/main/kotlin/com/example/attendanceapimono/application/dto/user/CreateUser.kt
@@ -0,0 +1,68 @@
+package com.example.attendanceapimono.application.dto.user
+
+import com.example.attendanceapimono.domain.user.*
+import io.swagger.v3.oas.annotations.media.Schema
+import lombok.extern.slf4j.Slf4j
+import org.hibernate.validator.constraints.Length
+import java.time.LocalDateTime
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotEmpty
+
+@Schema(
+    title = "회원가입",
+    description = "",
+    example = CreateUser.Example,
+)
+data class CreateUser(
+    @Schema(description = "소셜 토큰")
+    @field:NotEmpty
+    val token: String,
+
+    @Schema(description = "소설 타입")
+    val type: SocialType,
+    
+    @Schema(description = "이메일 주소")
+    @field:Email
+    val email: String,
+
+    @Schema(description = "이름(성)")
+    @field:Length(min = 1, max = 20)
+    val lastName: String,
+
+    @Schema(description = "이름")
+    @field:Length(min = 1, max = 20)
+    val firstName: String,
+
+    @Schema(description = "직군 포지션")
+    val position: UserPosition,
+) {
+    fun entity() = User(
+        id = UUID.randomUUID(),
+        state = UserState.NORMAL,
+        role = UserRole.MEMBER,
+        position = this.position,
+        generationID = 6, // TODO 별도의 제네레이션 테이블로 처리 하는게 좋을 듯함
+        firstName = this.firstName,
+        lastName = this.lastName,
+        email = this.email,
+        createdAt = LocalDateTime.now(),
+        updatedAt = LocalDateTime.now(),
+    )
+
+    companion object {
+        const val Example = """
+            {
+                "token": "social_token",
+                "type": "GOOGLE",
+                "email": "dddstudy1@gmail.com",
+                "lastName": "이",
+                "firstName": "재성",
+                "position": "BACKEND"
+            }
+        """
+    }
+}
