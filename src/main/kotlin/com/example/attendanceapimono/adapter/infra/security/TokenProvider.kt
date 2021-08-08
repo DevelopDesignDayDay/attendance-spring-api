109d9f66baef450bb3c1ae04efbff1eacda4aa74
Lee Jaeseong <fg9658@gmail.com>
2021-08-09 00:16:51 +0900

    refactor: 불필요한 세미콜론 및 사용하지 않는 함수 제거, role 리스트로 받을수 있게끔 변경, UserPrinciple->UserPrincipal(오타수정), 토큰 인증 부분 기존에 있는 UserPrincipal로 사용

--- src/main/kotlin/com/example/attendanceapimono/adapter/infra/security/TokenProvider.kt
+++ src/main/kotlin/com/example/attendanceapimono/adapter/infra/security/TokenProvider.kt
@@ -1,16 +1,13 @@
 package com.example.attendanceapimono.adapter.infra.security
 
-import com.example.attendanceapimono.util.log
 import com.example.attendanceapimono.util.tryOrNull
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.security.Keys
-import io.jsonwebtoken.security.SignatureException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
-import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Component
 import java.security.Key
 import java.util.*
 
@@ -21,10 +18,10 @@ class TokenProvider(authConfig: AuthProperties) {
 
     private fun getClaims(token: String) = Jwts
         .parserBuilder()
-        .setSigningKey(this.key)
+        .setSigningKey(key)
         .build()
         .parseClaimsJws(token)
-        .body;
+        .body
 
     fun createToken(userDetails: UserDetails): String {
         val now = Date()
@@ -34,45 +31,25 @@ class TokenProvider(authConfig: AuthProperties) {
             .setSubject(userDetails.username)
             .addClaims(mapOf(SecurityConst.CLAIMS_ROLE to userDetails.authorities))
             .setIssuer(SecurityConst.ADDRESS_URL)
-            .setIssuedAt(Date())
+            .setIssuedAt(now)
             .setExpiration(expiredDate)
-            .signWith(this.key, SignatureAlgorithm.HS512)
+            .signWith(key, SignatureAlgorithm.HS512)
             .compact();
     }
 
-    fun getUserId(token: String): UUID = UUID.fromString(this.getClaims(token).subject)
+    //TODO 사용하는건가ㅣ
+    fun getUserId(token: String): UUID = UUID.fromString(getClaims(token).subject)
 
     fun getAuthentication(token: String) = tryOrNull<Authentication> {
-        val claims = Jwts.parserBuilder()
-            .setSigningKey(key)
-            .build()
-            .parseClaimsJws(token)
-            .body
+        val claims = getClaims(token)
 
-        val authorities = (claims[SecurityConst.CLAIMS_ROLE] as ArrayList<*>).map { SimpleGrantedAuthority(it as String) }
+        val roles = (claims[SecurityConst.CLAIMS_ROLE] as ArrayList<*>)
+            .mapNotNull { RoleAdapter.valueOf(it as String) }
 
         return UsernamePasswordAuthenticationToken(
-            User(claims.subject, "", authorities),
+            UserPrincipal(UUID.fromString(claims.subject), roles),
             token,
-            authorities
+            roles
         )
-    }
-
-    fun validateToken(authToken: String): Boolean {
-        try {
-            Jwts.parserBuilder().setSigningKey(this.key).build().parseClaimsJws(authToken)
-            return true;
-        } catch (ex: SignatureException) {
-            log.error("Invalid JWT signature")
-        } catch (ex: MalformedJwtException) {
-            log.error("Invalid JWT token")
-        } catch (ex: ExpiredJwtException) {
-            log.error("Expired JWT token")
-        } catch (ex: UnsupportedJwtException) {
-            log.error("Unsupported JWT token")
-        } catch (ex: IllegalArgumentException) {
-            log.error("JWT claims string is empty.")
-        }
-        return false;
     }
 }
