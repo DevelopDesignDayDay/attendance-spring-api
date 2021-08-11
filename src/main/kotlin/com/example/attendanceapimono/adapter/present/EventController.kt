0aa7266c2f2d1cb235ec5b654fc0a16953d590cc
RbertKo <myeongsku@gmail.com>
2021-08-12 02:00:13 +0900

    feat: create EventController

---
+++ src/main/kotlin/com/example/attendanceapimono/adapter/present/EventController.kt
@@ -0,0 +1,8 @@
+package com.example.attendanceapimono.adapter.present
+
+import com.example.attendanceapimono.adapter.present.api.EventAPI
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class EventController: EventAPI {
+}
