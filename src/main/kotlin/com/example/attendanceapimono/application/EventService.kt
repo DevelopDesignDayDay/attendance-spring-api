93c7f1b68ce4a477d8bb093d0f453bfcf271c74d
RbertKo <myeongsku@gmail.com>
2021-08-13 00:54:17 +0900

    feat: add createEvent method

--- src/main/kotlin/com/example/attendanceapimono/application/EventService.kt
+++ src/main/kotlin/com/example/attendanceapimono/application/EventService.kt
@@ -1,10 +1,21 @@
 package com.example.attendanceapimono.application
 
+import com.example.attendanceapimono.application.dto.event.CreateEventRequest
+import com.example.attendanceapimono.application.dto.event.CreateEventResponse
+import com.example.attendanceapimono.domain.event.EventRepository
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
 
 @Service
-class EventService {
-    fun createEvent() {
+class EventService (
+  private val eventRepository: EventRepository
+) {
+    fun createEvent(dto: CreateEventRequest): CreateEventResponse = runBlocking {
+        val event = dto.entity()
 
+        eventRepository.save(event)
+
+        CreateEventResponse(event)
     }
 }
