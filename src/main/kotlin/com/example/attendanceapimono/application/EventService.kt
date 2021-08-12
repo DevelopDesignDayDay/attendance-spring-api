8d891b31021aaf5e054522125371c6ba3d99ce74
RbertKo <myeongsku@gmail.com>
2021-08-13 02:38:19 +0900

    feat: add createEvent Svc

--- src/main/kotlin/com/example/attendanceapimono/application/EventService.kt
+++ src/main/kotlin/com/example/attendanceapimono/application/EventService.kt
@@ -6,16 +6,18 @@ import com.example.attendanceapimono.domain.event.EventRepository
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class EventService (
   private val eventRepository: EventRepository
 ) {
+    @Transactional
     fun createEvent(dto: CreateEventRequest): CreateEventResponse = runBlocking {
         val event = dto.entity()
 
         eventRepository.save(event)
 
-        CreateEventResponse(event)
+        CreateEventResponse(eventId = event.id)
     }
 }
