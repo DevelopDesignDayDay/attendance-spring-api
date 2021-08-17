3ac445eff7522fae0e1885d40c9847687685a1b0
RbertKo <myeongsku@gmail.com>
2021-08-17 23:51:35 +0900

    feat: add GetEventList

--- src/main/kotlin/com/example/attendanceapimono/adapter/present/EventController.kt
+++ src/main/kotlin/com/example/attendanceapimono/adapter/present/EventController.kt
@@ -4,6 +4,7 @@ import com.example.attendanceapimono.adapter.present.api.EventAPI
 import com.example.attendanceapimono.application.EventService
 import com.example.attendanceapimono.application.dto.event.CreateEventRequest
 import com.example.attendanceapimono.application.dto.event.CreateEventResponse
+import com.example.attendanceapimono.application.dto.event.GetEventListResponse
 import com.example.attendanceapimono.application.exception.handleValidationCatch
 import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.web.bind.annotation.RestController
@@ -15,5 +16,9 @@ class EventController (private val eventService: EventService): EventAPI {
         return body.handleValidationCatch()
             .map(eventService::createEvent)
             .awaitSingle()
+    }
+
+    override suspend fun getEventList(): GetEventListResponse {
+        return eventService.getEventList()
     }
 }
